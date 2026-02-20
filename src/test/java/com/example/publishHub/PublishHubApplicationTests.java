efce225959ef71a05708cf768029aa6236443cd6
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-02-20 20:15:37 +0300

    Init commit

---
+++ src/test/java/com/example/publishHub/PublishHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publishHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
