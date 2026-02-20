0b1260f6bf73960c3d99df0c0690c2c6ebd6c41a
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-02-20 23:14:00 +0300

    comment entity

---
+++ src/main/java/com/example/publishHub/entity/CommentEntity.java
@@ -0,0 +1,72 @@
+package com.example.publishHub.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "content",
+            nullable = false
+    )
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "post_id",
+            nullable = false,
+            referencedColumnName = "id"
+    )
+    private PostEntity post;
+
+    @Column(
+            name = "created_at",
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            name = "approved",
+            nullable = false
+    )
+    private boolean approved;
+
+    @PrePersist
+    public void created() {
+        this.createdAt = LocalDateTime.now();
+        this.approved = false;
+    }
+}
