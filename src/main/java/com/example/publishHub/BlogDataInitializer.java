90811fec3b2962cc4f56d576dd98190e5bbf60dc
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-02-27 19:50:36 +0300

    initializer in dev-profile

--- src/main/java/com/example/publishHub/BlogDataInitializer.java
+++ src/main/java/com/example/publishHub/BlogDataInitializer.java
@@ -16,6 +16,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -29,6 +30,7 @@ import java.util.stream.IntStream;
 
 
 @Component
+@Profile("dev")
 @RequiredArgsConstructor
 public class BlogDataInitializer {
     private static final Logger log = LogManager.getLogger(BlogDataInitializer.class);
